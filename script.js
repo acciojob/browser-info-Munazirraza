39c30c6d6678500e1598499c1aacdbc37d11e671
MUNAZIR <60435223+Munazirraza@users.noreply.github.com>
2023-07-18 20:10:58 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,10 +1,16 @@
 //your JS code here. If required.
-
-let info = document.getElementById("browser-info");
-
-
-// info.innerText= "You are using " + navigator.userAgent;
-
-// info.innerText= "You are using " + navigator.userAgent  + " version " // passing in with this as well
-
-info.innerText = "You are using " + navigator.appName + " version " + navigator.appVersion;
+function livetime() {
+  let date = new Date();
+  let day = date.getDate();
+  let month = date.getMonth() + 1;
+  let year = date.getFullYear();
+  let time = date.toLocaleTimeString('en-US', { hour: 'numeric', minute: 'numeric', second: 'numeric', hour12: true });
+  let cudate = `${month}/${day}/${year}`;
+  document.getElementById('timer').textContent = cudate + ', ' + time;
+}
+function startTimer() {
+  livetime();
+  setInterval(livetime, 1000); 
+}
+ 
+window.addEventListener('load', startTimer);
